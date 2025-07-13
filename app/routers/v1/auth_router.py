20c612fca1acd2d7fe270f1d1c749172105fb555
Setsiri <setsiriaunsiam@gmail.com>
2025-07-14 06:28:56 +0700

    connect auth_router with db

--- app/routers/v1/auth_router.py
+++ app/routers/v1/auth_router.py
@@ -1,14 +1,15 @@
 from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
+from sqlmodel import Session
 from ...auth.authentication import oauth2_scheme, get_current_user, create_access_token
 from ...models.user_model import User
-from ...data.users import users_db
+from ...database import SessionDep
 
 router = APIRouter(tags=["authentication"])
 
 @router.post("/token")
-async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
-    user = users_db.get(form_data.username)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), session: Session = Depends(SessionDep)):
+    user = session.get(form_data.username)
     if user is None or user.password != form_data.password:
         raise HTTPException(status_code=400, detail="Incorrect username or password")
     access_token = create_access_token(data={"sub": user.username})
